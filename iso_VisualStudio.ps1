e3e6cb3d5f8889bce1484365ef27684b9809f0d3
Zenobius Jiricek <airtonix@gmail.com>
2017-06-30 12:09:30 +0930

    Create iso_VisualStudio.ps1

---
+++ iso_VisualStudio.ps1
@@ -0,0 +1,21 @@
+Import-Module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1"
+
+function installFromIso ($name, $source, $silentArgs, $setupCommand) {
+    
+	$mountVolume = mount-diskimage $source -PassThru
+	$driveLetter = ($mountVolume | Get-Volume).DriveLetter
+    $setupPath = "$($driveLetter):\$setupCommand"
+	Install-ChocolateyInstallPackage -PackageName $name -FileType 'exe' -SilentArgs $silentArgs "$($driveLetter):\setup.exe"
+    dismount-diskimage $source
+}
+
+# SQL Server
+$SqlServerSilentArgs = "/IACCEPTSQLSERVERLICENSETERMS /Q /ACTION=install /INSTANCEID=SQLEXPRESS /INSTANCENAME=SQLEXPRESS /UPDATEENABLED=FALSE"
+$SqlServerIsoPath = "SQL_Svr_Web_Ed_2014w_SP1_64Bit_English" "\\fileserver\software\windows\isos\SW_DVD9_SQL_Svr_Web_Ed_2014w_SP1_64Bit_English_-2_MLF_X20-29022.ISO"
+installFromIso $SqlServerIsoPath $SqlServerSilentArgs "setup.exe"
+
+# Visual Studio
+$VisualStudioPackageName = "Visual_Studio_Pro_2015_English"
+$VisualStudioIsoPath = "\\fileserver\software\windows\isos\SW_DVD9_NTRL_Visual_Studio_Pro_2015_English_FPP_VL_MLF_X20-29901.ISO"
+$VisualStudioSilentArgs = "/quiet /full"
+installFromIso $VisualStudioPackageName $VisualStudioIsoPath $VisualStudioSilentArgs "vs_professional.exe"
