5042bdb9aff49b15b0bc9c56bbfdb8f10bb81c97
Alex Day <alexday360@gmail.com>
2017-07-14 17:37:08 +0930

    Fix: sublime text package manager installs manually rather than via cocolatey due to issues

--- software__sublimetext3.ps1
+++ software__sublimetext3.ps1
@@ -2,12 +2,15 @@ set-executionpolicy RemoteSigned
 
 
 choco install sublimetext3
-choco install sublimetext3.packagecontrol
-
 
 $sublimeConfig = "$env:APPDATA\Sublime Text 3"
 $packagesRoot = "$sublimeConfig\Installed Packages"
+$packageControlOut = "$packagesRoot\Package Control.sublime-package"
 $userPackages = "$sublimeConfig\Packages\User"
+
+
+# download Package Control to the Installed Packages folder
+iwr -useb https://packagecontrol.io/Package%20Control.sublime-package -OutFile $packageControlOut
 
 $packagesToInstall = @{
     installed_packages = @(
