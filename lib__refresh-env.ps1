fe669d111ecdb6ab6a38bce72edce8a1cd22fe4f
Zenobius Jiricek <airtonix@gmail.com>
2017-11-20 16:42:12 +1100

    Update lib__refresh-env.ps1

--- lib__refresh-env.ps1
+++ lib__refresh-env.ps1
@@ -4,9 +4,9 @@ function refresh-path {
 	   [Environment]::GetEnvironmentVariables($level).GetEnumerator() | % {
 	      # For Path variables, append the new values, if they're not already in there
 	      if($_.Name -match 'Path$') { 
-	         $_.Value = ($((Get-Content "Env:$($_.Name)") + ";$($_.Value)") -split ';' | Select -unique) -join ';'
+	         $_.Value = ($((Get-Content "Env:$_.Name") + ";$($_.Value)") -split ';' | Select -unique) -join ';'
 	      }
 	      $_
-	   } | Set-Content -Path { "Env:$($_.Name)" }
+	   } | Set-Content -Path { "Env:$_.Name" }
 	}
 }
