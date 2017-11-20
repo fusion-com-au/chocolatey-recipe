18b2296a0b8e7c7b928ceb78a9ab98fb9d2b62ea
Zenobius Jiricek <airtonix@gmail.com>
2017-11-20 22:40:51 +1100

    Update boxstarter-profile--web-development.ps1

--- boxstarter-profile--web-development.ps1
+++ boxstarter-profile--web-development.ps1
@@ -1,2 +1,2 @@
 . { iwr -useb http://boxstarter.org/bootstrapper.ps1 } | iex; get-boxstarter -Force
-Install-BoxstarterPackage -PackageName https://github.com/fusion-com-au/dev-workstation-boxstarter/workstation_web.ps1
+Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/fusion-com-au/dev-workstation-boxstarter/master/workstation_web.ps1
