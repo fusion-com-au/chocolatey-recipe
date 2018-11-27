832d5cc8cac41730e0cd7249bb908fc4754daa1a
alex-day <AlexDay360@gmail.com>
2018-11-27 15:43:53 +1030

    Update software__utilities-generic.ps1

--- software__utilities-generic.ps1
+++ software__utilities-generic.ps1
@@ -1,11 +1,11 @@
 set-executionpolicy RemoteSigned
 
 
-choco install f.lux
-choco install zoomit
+# choco install f.lux
+# choco install zoomit
 choco install sharex
 choco install bonjour
 choco install 7zip
 choco install vlc
-choco install enpass.install
-choco install autohotkey.portable
+# choco install enpass.install
+# choco install autohotkey.portable
