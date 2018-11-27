bd710b2563f01af4a7694171b0bf79a438dfba20
alex-day <AlexDay360@gmail.com>
2018-11-27 15:45:05 +1030

    Update software__utilities-dev.ps1

--- software__utilities-dev.ps1
+++ software__utilities-dev.ps1
@@ -1,10 +1,10 @@
 set-executionpolicy RemoteSigned
 
 
-choco install licecap
+# choco install licecap
 choco install sourcetree
 choco install winmerge
-choco install fiddler4
-choco install astrogrep
-choco install procexp
-choco install devdocs-app
+# choco install fiddler4
+# choco install astrogrep
+# choco install procexp
+# choco install devdocs-app
