7705cbc25185405306d24c1eb35a7214be829e95
alex-day <AlexDay360@gmail.com>
2018-11-27 15:42:00 +1030

    Update workstation.ps1

--- workstation.ps1
+++ workstation.ps1
@@ -4,7 +4,8 @@ set-executionpolicy RemoteSigned
 . { iwr -useb https://raw.githubusercontent.com/fusion-com-au/dev-workstation-boxstarter/master/base.ps1 } | iex;
 . { iwr -useb https://raw.githubusercontent.com/fusion-com-au/dev-workstation-boxstarter/master/software__windows.ps1 } | iex;
 . { iwr -useb https://raw.githubusercontent.com/fusion-com-au/dev-workstation-boxstarter/master/software__docker.ps1 } | iex;
-. { iwr -useb https://raw.githubusercontent.com/fusion-com-au/dev-workstation-boxstarter/master/software__browsers.ps1 } | iex;
+# Install browsers manually
+# . { iwr -useb https://raw.githubusercontent.com/fusion-com-au/dev-workstation-boxstarter/master/software__browsers.ps1 } | iex;
 . { iwr -useb https://raw.githubusercontent.com/fusion-com-au/dev-workstation-boxstarter/master/software__powershell.ps1 } | iex;
 
 . { iwr -useb https://raw.githubusercontent.com/fusion-com-au/dev-workstation-boxstarter/master/software__utilities-generic.ps1 } | iex;
